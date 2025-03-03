f53c40afd1479b3498ad5f2746001e7b68f03204
abhinav-1312 <abhinav@dkgrouplabs.com>
2025-03-03 16:08:25 +0530

    added reports

---
+++ src/pages/reports/CpReport.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import CustomReport from '../../components/DKG_Report';
+
+const CpReport = () => {
+  const api = "/astro-service/api/reports/cp/report"
+  const columns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      filterable: true
+    },
+    {
+      title: 'Material',
+      dataIndex: 'material',
+      key: 'material',
+      filterable: true
+    },
+    {
+      title: 'Material Category',
+      dataIndex: 'materialCategory',
+      key: 'materialCategory',
+      filterable: true
+    },
+    {
+      title: 'Material Sub-Category',
+      dataIndex: 'materialSubCategory',
+      key: 'materialSubCategory',
+      filterable: true
+    },
+    {
+      title: 'End User',
+      dataIndex: 'endUser',
+      key: 'endUser',
+      filterable: true
+    },
+    {
+      title: 'Value',
+      dataIndex: 'value',
+      key: 'value',
+      filterable: true,
+      render: (value) => `$${value.toFixed(2)}`, // Format as currency
+    },
+    {
+      title: 'Paid To',
+      dataIndex: 'paidTo',
+      key: 'paidTo',
+      filterable: true
+    },
+    {
+      title: 'Vendor Name',
+      dataIndex: 'vendorName',
+      key: 'vendorName',
+      filterable: true
+    },
+    {
+      title: 'Project',
+      dataIndex: 'project',
+      key: 'project',
+      filterable: true
+    },
+  ];
+  
+  return <CustomReport api={api} columns={columns} title="Contingency Purchase Report" />
+}
+
+export default CpReport
---
+++ src/pages/reports/ProcurementActivityReport.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import CustomReport from '../../components/DKG_Report';
+
+const ProcurementActivityReport = () => {
+  const api = "/astro-service/api/reports"
+  const columns = [
+    {
+      title: "Order ID",
+      dataIndex: "orderId",
+      key: "orderId",
+      filterable: true
+    },
+    {
+      title: "Gem or Non-Gem",
+      dataIndex: "gemOrNonGem",
+      key: "gemOrNonGem",
+      filterable: true
+    },
+    {
+      title: "Indentor",
+      dataIndex: "indentor",
+      key: "indentor",
+      filterable: true
+    },
+    {
+      title: "Value",
+      dataIndex: "value",
+      key: "value",
+      filterable: true
+    },
+    {
+      title: "Description of Goods",
+      dataIndex: "descriptionOfGoods",
+      key: "descriptionOfGoods",
+      filterable: true
+    },
+    {
+      title: "Vendor Name",
+      dataIndex: "vendorName",
+      key: "vendorName",
+      filterable: true
+    },
+  ];
+  return <CustomReport api={api} columns={columns} title="Procurement Activity Report" />
+}
+
+export default ProcurementActivityReport
