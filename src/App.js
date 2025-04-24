79af331a2cfa114a2402702cbb2761e3adc97f3a
kudaykiran-99 <uday@dkgrouplabs.com>
2025-04-24 15:59:53 +0530

    Added vendor data popup in queue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux';
 import { fetchMasters } from './store/slice/masterSlice';
 
 axios.defaults.baseURL="http://103.181.158.220:8081/astro-service"
-// axios.defaults.baseURL="http://localhost:8081/astro-service"
+//axios.defaults.baseURL="http://localhost:8081/astro-service"
 
 function App() {
   const dispatch = useDispatch();
